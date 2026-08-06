3cbfc3f60ad42c6f03a4366464f8535da97a1f09
Ivan Sitohang <sitohang@equinoxshipping.co.id>
2026-08-07 01:00:03 +0700

    update root path

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\KuitansiController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/kuitansi', [KuitansiController::class, 'create'])->name('kuitansi.create');
+Route::get('/', [KuitansiController::class, 'create'])->name('kuitansi.create');
 Route::post('/kuitansi/pdf', [KuitansiController::class, 'generatePdf'])->name('kuitansi.pdf');
